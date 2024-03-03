96d72b83c2c363f2ddb62629fcc764f1fc3f2733
Sergey Biryukov <serbathome@gmail.com>
2024-03-03 20:57:21 +0100

    reworked shared resources

--- project/main.tf
+++ project/main.tf
@@ -1,14 +1,19 @@
+resource "azurerm_resource_group" "group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 module "web_app" {
   source              = "./modules/webapp"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
   service_plan_name   = var.service_plan_name
   web_app_name        = var.web_app_name
 }
 
 module "storage_account" {
   source               = "./modules/storage_account"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
+  resource_group_name  = azurerm_resource_group.group.name
+  location             = azurerm_resource_group.group.location
   storage_account_name = var.storage_account_name
 }
--- project/modules/storage_account/main.tf
+++ project/modules/storage_account/main.tf
@@ -7,10 +7,14 @@ resource "random_string" "random" {
   lower   = true
 }
 
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_storage_account" "storage_account" {
   name                     = "${var.storage_account_name}-${random_string.random.result}"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  resource_group_name      = data.azurerm_resource_group.resource_group.name
+  location                 = data.azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
