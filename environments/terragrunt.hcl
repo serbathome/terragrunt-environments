7ae7016c9aa3fed43436d51fd8af9f38b026cc14
Sergey Biryukov <serbathome@gmail.com>
2024-03-03 22:25:04 +0100

    provider fix

--- environments/terragrunt.hcl
+++ environments/terragrunt.hcl
@@ -12,3 +12,13 @@ terraform {
 }
 EOF
 }
+
+generate "provider" {
+  path      = "provider.tf"
+  if_exists = "overwrite_terragrunt"
+  contents = <<EOF
+provider "azurerm" {
+  features {}
+}
+EOF
+}
--- project/storage_account/modules/storage_account/main.tf
+++ project/storage_account/modules/storage_account/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.storage_account_name}-${random_string.random.result}"
+  name                     = "${var.storage_account_name}${random_string.random.result}"
   resource_group_name      = azurerm_resource_group.group.name
   location                 = azurerm_resource_group.group.location
   account_tier             = "Standard"
