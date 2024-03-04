2bda733de0f300c6f4a29a401d9d793ae9813dd7
Sergei Biryukov <svbiryukov@luxoft.com>
2024-03-04 19:38:47 +0100

    removed projects folder

--- project/storage_account/main.tf
+++
@@ -1,7 +0,0 @@
-
-module "storage_account" {
-  source               = "./modules/storage_account"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  storage_account_name = var.storage_account_name
-}
--- project/webapp/main.tf
+++
@@ -1,9 +0,0 @@
-
-module "web_app" {
-  source              = "./modules/webapp"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  service_plan_name   = var.service_plan_name
-  web_app_name        = var.web_app_name
-}
-
